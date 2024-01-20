c1c1bf925f18a021a037f9a947de7294fc871b2d
Rob Puncel <rpuncel@gmail.com>
2024-01-20 15:40:25 -0800

    add slightly more helpful docs

--- src/michar/cli/app/cmd/fbi.py
+++ src/michar/cli/app/cmd/fbi.py
@@ -5,7 +5,15 @@ from michar.api.fbi.client import FBIClient
 @gooza.group()
 @click.pass_context
 def fbi(ctx: click.Context):
-    """FBI, open up!"""
+    """
+    FBI, open up!
+    
+    Subcommands will interact with data from the
+    [FBI Crime Data API](https://cde.ucr.cjis.gov/LATEST/webapp/#/pages/docApi)
+
+    To authenticate, set an environment variable named FBI_API_KEY.
+    You can request an API_KEY here https://api.data.gov/signup/
+    """
     ctx.obj = FBIClient()
 
 @fbi.command
