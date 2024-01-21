4143727f5fe5790d5b15bc308ab943e9144af57a
Boris Borelly <borisborelly@gmail.com>
2024-01-20 17:47:42 -0800

    crawler 4 dummies

--- src/michar/cli/app/cmd/crawler.py
+++ src/michar/cli/app/cmd/crawler.py
@@ -2,19 +2,6 @@ import rich_click as click
 import michar.api.util as util
 from typing import List
 from michar.cli.app.michar import gooza
-import requests
-from bs4 import BeautifulSoup, Tag
-from selenium import webdriver
-from selenium.webdriver.firefox.service import Service
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.firefox.service import Service as FirefoxService
-from webdriver_manager.firefox import GeckoDriverManager
-
-from selenium.webdriver.chrome.service import Service as BraveService
-from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.core.os_manager import ChromeType
 
 
 
@@ -27,60 +14,12 @@ def crawl(url: str):
     crawl websites for data
     """
     log = util.get_logger()
-    log.info(f"crawling {url}")
+    krawler: util.Krawl = util.get_crawler(url=url)
 
-    #page = requests.get(url)
 
-    driver = webdriver.Firefox()
-    #driver = webdriver.Chrome(service=BraveService(ChromeDriverManager(chrome_type=ChromeType.BRAVE).install()))
+    content_div = krawler.get_div('ctl00_ContentPlaceHolder1_divGrid')
+    log.info(content_div)
 
-    # Navigate to the .aspx page
-    log.info(driver)
-    driver.get(url)
-    try:
-        # Wait for the dynamic content to load (adjust the timeout as needed)
-        # https://longbeach.legistar.com/calendar.aspx
-        # TODO figure out how to crawl this javascript crap
-        element = WebDriverWait(driver, 20).until(
-            EC.presence_of_element_located((By.ID, 'ctl00_ContentPlaceHolder1_divGrid'))
-        )
 
-        # Extract data from the dynamic element
-        dynamic_content = driver.find_element(By.ID, 'your_dynamic_element_id').text
-        print(dynamic_content)
-
-    finally:
-        # Close the browser window
-        driver.quit()
-
+    krawler.shutdown()
     
-
-
-
-
-
-
-
-
-    # soup = BeautifulSoup(page.content, "html.parser")
-    # log.debug(soup)
-
-    # data: Tag = soup.find(id="ctl00_ContentPlaceHolder1_gridCalendar")
-    # log.info(data.prettify())
-
-    # master_table: Tag = data.find(class_='rgMasterTable')
-    # print("AAA")
-    # log.info(master_table)
-
-    # entries: List[Tag] = master_table.find_all('tr')
-    # log.info("SDSSSAAS")
-    # log.info(entries)
-
-    # rows: List[Tag] = entries.find_all('tr')
-    # for row in rows:
-    #     # Extract data from each cell in the row
-    #     cells = row.find_all(['th', 'td'])
-    #     row_data = [cell.text.strip() for cell in cells]
-        
-    #     # Process or print the row data
-    #     print(row_data)
