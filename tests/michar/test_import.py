fbb22290e3ada7cfac98da107576123d3ec3a5c4
Boris Borelly <borisborelly@gmail.com>
2024-01-20 18:16:45 -0800

    fixtest

--- tests/michar/test_import.py
+++ tests/michar/test_import.py
@@ -5,4 +5,4 @@ class testMichi(unittest.TestCase):
     def testVersion(self):
         print(f"{michar.__name__=} {michar.__version__=}")
         self.assertIsNotNone(michar.__name__)
-        self.assertIsNotNone(michar.version)
+        self.assertIsNotNone(michar.__version__)
