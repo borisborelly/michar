678674a61903d2e7641d546395173e0da1d69fca
Boris Borelly <borisborelly@gmail.com>
2024-01-26 12:01:10 -0800

    pydocs

--- src/michar/__init__.py
+++
@@ -1,7 +0,0 @@
-__name__ = "michar"
-__all__ = ["api", "cli"]
-__authors__ = ["Boris", "Rob"]
-__copyright__ = '2024, Boris, Rob'
-import importlib.metadata
-
-__version__ = importlib.metadata.version(__name__)
--- tests/michar/api/test_util.py
+++
@@ -1,6 +0,0 @@
-import unittest
-import michar
-
-class testMicharUtil(unittest.TestCase):
-    def test_noop(self):
-        print(f"blah todo")
